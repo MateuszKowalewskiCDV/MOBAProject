fcb1e03341aed365f84ce7c1bdfd989606a1c068
Bartek <bartoszkialka4@gmail.com>
2021-11-15 03:10:27 +0100

    menu update

---
+++ InzynierkaProject/Assets/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   public void Play(){
+   
+   SceneManager.LoadScene("host_finder",LoadSceneMode.Single);
+    }
+     public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ InzynierkaProject/Assets/chose.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class chose : MonoBehaviour
+{
+    public void Join(){
+
+   
+   SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+     public void Host()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void Back()
+    {
+       SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+
+
+
+}
